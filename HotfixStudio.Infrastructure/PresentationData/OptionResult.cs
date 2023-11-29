e3d71de714600330511d0ca8e40bafc40d06c1b8
Marius <marius@fjeld.et>
2023-11-29 11:20:37 +0100

    Setup base select and other restructures

---
+++ HotfixStudio.Infrastructure/PresentationData/OptionResult.cs
@@ -0,0 +1,18 @@
+﻿namespace HotfixStudio.Infrastructure.PresentationData
+{
+    public class OptionResult<T>
+    {
+        public Dictionary<T, string> Options { get; set; } = new();
+        public int CurrentPageIndex { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRows { get; set; }
+        public int TotalPages
+        {
+            get
+            {
+                return PageSize > 0 ? (int)TotalRows / PageSize : 0;
+            }
+        }
+    }
+}
