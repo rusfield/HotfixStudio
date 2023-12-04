8a7e88c825cbde5a88e9a4c37c2409ecd38dab00
Marius <marius@fjeld.et>
2023-12-04 15:33:42 +0100

    Add Resize base and enable drag on Dropdown

---
+++ HotfixStudio.Presentation/Enums/DragBarType.cs
@@ -0,0 +1,10 @@
+﻿namespace HotfixStudio.Presentation.Enums
+{
+    public enum DragBarType
+    {
+        HORIZONTAL_TOP,
+        HORIZONTAL_BOTTOM,
+        VERTICAL_LEFT,
+        VERTICAL_RIGHT
+    }
+}
