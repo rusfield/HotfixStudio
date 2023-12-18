31dae918fa40e076cdabcaf70eec8028515be286
Marius <marius@fjeld.et>
2023-12-18 14:00:20 +0100

    Restructure

---
+++ HotfixStudio.Presentation/ComponentModels/MenuObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HotfixStudio.Presentation.ComponentModels
+{
+    public class MenuObject
+    {
+        public string Header { get; set; }
+        public string IconName { get; set; }    
+        public List<Section> Sections { get; set; } = new();
+
+        public void AddSection(Dictionary<string, EventCallback> items)
+        {
+            Sections.Add(new()
+            {
+                Items = items
+            });
+        }
+
+        public int GetTotalItems()
+        {
+            return Sections.Sum(section => section.Items.Count);
+        }
+
+        public class Section
+        {
+            public Dictionary<string, EventCallback> Items { get; set; }
+        }
+    }
+}
